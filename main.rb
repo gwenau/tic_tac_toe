033e56e8988203eeedb2a8d817557b3ce3d22942
Gwen Au <quyen_au@hotmail.com>
2014-10-25 20:29:19 +0100

    'X' is updated in the array.

--- main.rb
+++ main.rb
@@ -1,42 +1,42 @@
+def intialize(value)
+  @array = value
+  @move = value
+end
 
+def new_game
+  @array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+end
 
-  def intialize(value)
-    @array = value
-    @move = value
-  end
+# def move(move_x)
+#   @move = move_x
+# end
 
-  def new_game
-    @array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  end
+# Had to have this here to initialize the variable.
+@array = []
+@move
+# Have to put new_game here to update the variable above.
+new_game
 
-  # def move(move_x)
-  #   @move = move_x
-  # end
+puts "\n-------"
+print "|"
+@array[0..2].each do |ak|
+  print "#{ak}|"
+end
+puts "\n-------"
+print "|"
+@array[3..5].each do |ak| 
+  print "#{ak}|" 
+end
+puts "\n-------"
+print "|"
+@array[6..8].each do |ak| 
+  print "#{ak}|" 
+end
+puts "\n-------"
+puts "Select a square number"
+@move = gets.to_i
+@array[@move-1] = "X"
 
-  # Had to have this here to initialize the variable.
-  @array = []
-  @move
-  # Have to put new_game here to update the variable above.
-  new_game
-
-  puts "\n-------"
-  print "|"
-  @array[0..2].each do |ak|
-    print "#{ak}|"
-  end
-  puts "\n-------"
-  print "|"
-  @array[3..5].each do |ak| 
-    print "#{ak}|" 
-  end
-  puts "\n-------"
-  print "|"
-  @array[6..8].each do |ak| 
-    print "#{ak}|" 
-  end
-  puts "\n-------"
-  puts "Select a square number"
-  @move = gets 
-  print "You have selected " + @move
+puts @array
 
 
