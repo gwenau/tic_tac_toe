fc3ab8338f8d8c3528e932a2cb578e07ba14248b
Gwen Au <quyen_au@hotmail.com>
2014-10-25 20:08:40 +0100

    The game table layout is ok.

--- main.rb
+++ main.rb
@@ -1,10 +1,32 @@
 
-def intialize
-  @hash = {}
-end
 
-def new_game
-  @hash = {one: "1", two: "2", three: "3", four: "4", five: "5", six: "6", seven: "7", eight: "8", nine: "9"}
-end
+  def intialize(value)
+    @array = value
+  end
 
-puts new_game
+  def new_game
+    @array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  end
+
+  # Had to have this here to initialize the variable.
+  @array = []
+
+  # Have to put new_game here to update the variable above.
+  new_game
+
+  puts "\n-------"
+  print "|"
+  @array[0..2].each do |ak|
+    print "#{ak}|"
+  end
+  puts "\n-------"
+  print "|"
+  @array[3..5].each do |ak| 
+    print "#{ak}|" 
+  end
+  puts "\n-------"
+  print "|"
+  @array[6..8].each do |ak| 
+    print "#{ak}|" 
+  end
+  puts "\n-------"
