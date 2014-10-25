64df8f665748cb5ac05616a016ed9ffd2b94df13
Gwen Au <quyen_au@hotmail.com>
2014-10-25 21:38:21 +0100

    X can win the game and exit.

--- main.rb
+++ main.rb
@@ -17,10 +17,6 @@ end
 # Have to put new_game here to update the variable above.
 new_game
 
-def winning
-
-end
-
 def x_move
   puts "\n-------"
   print "|"
@@ -40,18 +36,56 @@ def x_move
   puts "\n-------"
 end
 
+def won_x
+  if @array[0] + @array[1] + @array[2] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[3] + @array[4] + @array[5] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[6] + @array[7] + @array[8] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[0] + @array[3] + @array[6] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[1] + @array[4] + @array[7] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[2] + @array[5] + @array[8] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[0] + @array[4] + @array[8] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  elsif @array[2] + @array[4] + @array[6] == "XXX"
+    puts "Player 1 wins the game!"
+    exit
+  else
+  x_move
+  end
+end
+
 def player_1
   x_move
   puts "1st player select a square number"
   @move = gets.to_i
+  # if @array[@move-1] == "X" || "O"
+  #   puts "Sorry that square has already been taken, please select another square"
+  # end
   @array[@move-1] = "X"
+  won_x
 end
 
 def player_2
   x_move
   puts "2nd player select a square number"
   @move = gets.to_i
+  # if @array[@move-1] == "X" || "O"
+  #   puts "Sorry that square has already been taken, please select another square"
+  # end
   @array[@move-1] = "O"
+  won_x
 end
 
 
