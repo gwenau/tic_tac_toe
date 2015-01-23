fdbdcea5f6df8392d3b5477f0b11ce8eda013608
Gwen Au <quyen_au@hotmail.com>
2015-01-23 16:28:33 +0300

    Everything working before structure refactoring.

--- main.rb
+++ main.rb
@@ -1,31 +1,13 @@
-# The below method might be needed when you are inside a Class but outside, you don't need it.
-# def intialize(value)
-#   puts "Whatever"
-#   @array = value
-#   @move = value
-#   @player = value
-# end
-
 def new_game
   @array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 end
 
-# Had to have this here to initialize the variable.
-# @array = []
-# @move
 @player = [ "player 1", "player 2" ]
-# player_1 = {token: "X", score: 0}
-# player_2 = {token: "O", score: 0}
-# @player << player_1
-# @player << player_2
 
-# Have to put new_game here to update the variable above.
 new_game
 
 def game_board
-
   lines = [0..2, 3..5, 6..8]
-
   lines.each do |line|
     puts "\n-------"
     print "|"
@@ -34,15 +16,11 @@ def game_board
     end
   end
   puts "\n-------"
-
 end
 
-
 def winner (player)
-
   winning_combination = [[0, 1, 2], [3, 4, 5,], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   y = @array
-
   winning_combination.each do |wc|
     if y[wc[0]] + y[wc[1]] + y[wc[2]] == "XXX"
       game_board
@@ -55,7 +33,6 @@ def winner (player)
     else
     end
   end
-
 end
 
 def player_move(player)
@@ -73,16 +50,9 @@ def player_move(player)
   end
 end
 
+(0..1).cycle(4) do |move|
+  player_move(@player[move])
+end
 player_move(@player[0])
-player_move(@player[1])
-player_move(@player[0])
-player_move(@player[1])
-player_move(@player[0])
-player_move(@player[1])
-player_move(@player[0])
-player_move(@player[1])
-player_move(@player[0])
-
 game_board
-
-# Need a draw method after nine moves have been used up. 
+puts "Draw!"
